b91b71effe1c9dec55578f15baa3afbe82b63a75
Paras rana <52912280+paras1904@users.noreply.github.com>
2021-05-18 13:55:02 +0530

    Update Faishon mnist with cnn.py

--- Faishon mnist with cnn.py
+++ Faishon mnist with cnn.py
@@ -1,22 +1,36 @@
-import keras as ks
-import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense,Conv2D,MaxPooling2D,Flatten
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import MaxPooling2D, Dropout, Conv2D, Dense, Flatten
+from tensorflow.keras.models import Sequential
+import matplotlib.pyplot as plt
 import numpy as np
-(trainX, trainY), (testX, testY) = ks.datasets.fashion_mnist.load_data()
-trainX =trainX.reshape(60000,28,28,1)
-testX = testX.reshape(10000,28,28,1)
-trainX = trainX.astype("float32") / 255.0
-testX = testX.astype("float32") / 255.0
+
+
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
+x_train = x_train.reshape(60000,28,28,1)
+x_test = x_test.reshape(10000,28,28,1)
+
+x_train = x_train.astype('float32')/255.
+x_test = x_test.astype('float32')/255.
+
+size = (28,28,1)
 
 model = Sequential([
-    Conv2D(64,(3,3),activation='relu',input_shape=(28,28,1)),
-    MaxPooling2D(2,2),
-    Conv2D(64,(3,3),activation='relu'),
-    MaxPooling2D(2,2),
+    Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu',use_bias=True,kernel_initializer='normal',kernel_regularizer='l1',bias_regularizer='l1',bias_initializer='zero',input_shape=size),
+    MaxPooling2D(pool_size=2,strides=1),
+    Conv2D(128,(3,3),strides=(1,1),padding='same',activation='relu',use_bias=True,kernel_initializer='normal',kernel_regularizer='l1',bias_regularizer='l1',bias_initializer='zero'),
+    MaxPooling2D(pool_size=2,strides=1),
+    Conv2D(64,(3,3),strides=(1,1),padding='same',activation='relu',use_bias=True,kernel_initializer='normal',kernel_regularizer='l1',bias_regularizer='l1',bias_initializer='zero'),
+    MaxPooling2D(pool_size=2,strides=1),
+    Conv2D(32,(3,3),strides=(1,1),padding='same',activation='relu',use_bias=True,kernel_initializer='normal',kernel_regularizer='l1',bias_regularizer='l1',bias_initializer='zero'),
+    MaxPooling2D(pool_size=2,strides=1),
+    Conv2D(16,(3,3),strides=(1,1),padding='same',activation='relu',use_bias=True,kernel_initializer='normal',kernel_regularizer='l1',bias_regularizer='l1',bias_initializer='zero'),
     Flatten(),
-    Dense(128,activation='relu'),
-    Dense(10,activation='softmax')
+    Dense(32,activation='relu',use_bias=True,kernel_initializer='normal',bias_initializer='zero',kernel_regularizer='l1',bias_regularizer='l1'),
+    Dropout(.2),
+    Dense(20,activation='relu',use_bias=True,kernel_initializer='normal',bias_initializer='zero',kernel_regularizer='l1',bias_regularizer='l1'),
+    Dropout(.2),
+    Dense(10,activation='softmax'),
 ])
-model.compile(optimizer="adam",metrics=['accuracy'],loss='sparse_categorical_crossentropy')
-model.fit(trainX,trainY,epochs=1,verbose=2)
+model.compile(optimizer='adam',loss='mean_squared_error',metrics=['accuracy'])
+model.fit(x_train,y_train)
+print(model.evaluate(x_test,y_test,batch_size=500,verbose=1))
